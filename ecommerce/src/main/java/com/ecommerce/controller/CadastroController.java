09bd767415e81e7123e56b2b14b7cdc7deee58eb
edumoritz <eduardo.moritz@hotmail.com>
2018-06-01 00:11:53 -0300

    Inserindo produto no banco

--- ecommerce/src/main/java/com/ecommerce/controller/CadastroController.java
+++ ecommerce/src/main/java/com/ecommerce/controller/CadastroController.java
@@ -1,14 +1,30 @@
 package com.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecommerce.domain.Produto;
+import com.ecommerce.service.ProdutoService;
 
 @Controller
+@RequestMapping("/cadastros")
 public class CadastroController {
 	
-	@GetMapping("/cadastro")
-	public String cadastro() {
-		return "/cadastro/cadastro";
+	@Autowired
+	private ProdutoService service;
+	
+	@GetMapping("/cadastrar")
+	public String cadastrar(Produto produto) {
+		return "/cadastro/produto";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Produto produto) {
+		service.salvar(produto);
+		return "redirect:/cadastros/cadastrar";
 	}
 
 
