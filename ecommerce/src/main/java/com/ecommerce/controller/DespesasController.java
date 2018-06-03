c31b83ed61d54f019639080bf7becea4a0c9b796
edumoritz <eduardo.moritz@hotmail.com>
2018-06-03 16:14:10 -0300

    Despesas incompleto

--- ecommerce/src/main/java/com/ecommerce/controller/DespesasController.java
+++ ecommerce/src/main/java/com/ecommerce/controller/DespesasController.java
@@ -1,7 +1,7 @@
 package com.ecommerce.controller;
 
 import java.math.BigDecimal;
-
+import java.math.RoundingMode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,27 +10,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecommerce.domain.Despesas;
+import com.ecommerce.domain.Produto;
 import com.ecommerce.service.ProdutoService;
 
 @Controller
 @RequestMapping("/despesas")
 public class DespesasController {
 	Despesas despesa = new Despesas();
+	Produto produto = new Produto();
 	BigDecimal rateio = new BigDecimal("0.00");
-	
+	BigDecimal result;
+
 	@Autowired
 	private ProdutoService service;
-	
+
 	@GetMapping("/listar")
 	public String listar(ModelMap model, Despesas despesa) {
 		model.addAttribute("produtos", service.buscarTodos());
 		return "/lista/lista";
 	}
-	
+
 	@GetMapping("/excluir/{id}")
-	public String excluir(@PathVariable("id") Long id, ModelMap model, Despesas despesa) {
+	public String excluir(@PathVariable("id") Long id, ModelMap model, Despesas despesas) {
 		service.excluir(id);
 		model.addAttribute("success", "Produto excluído com sucesso.");
 		return listar(model, despesa);
@@ -41,15 +45,60 @@ public class DespesasController {
 		model.addAttribute("produto", service.buscarPorId(id));
 		return "/cadastro/produto";
 	}
+
 	@PostMapping("/calcula")
-	public String calcula(Despesas despesa) {
-		if(despesa.getDespesa() == null || despesa.getMargem() == null) {
-			despesa.setDespesa(new BigDecimal("400.00"));
-			despesa.setMargem(10);
+	public String calcula(ModelMap model, Despesas despesa) {
+		BigDecimal m = new BigDecimal("0.10");
+		BigDecimal d = new BigDecimal("600.00");
+		if(despesa.getDespesa() == null) {
+			if(despesa.getMargem() == null) {
+				despesa.setDespesa(d);
+				despesa.setMargem(m);
+				calculaA(despesa);
+			} else {
+				calculaA(despesa);
+			}
 		} else {
-			
+			if(despesa.getMargem() == null) {
+				d = despesa.getDespesa();
+				despesa.setMargem(m);
+				calculaA(despesa);
+			} else {
+				m = despesa.getMargem();
+				calculaA(despesa);
+			}
 		}
-		return "/lista/lista";
+		
+		return "redirect:/despesas/listar";
 	}
+	
+	
+
+	private void calculaA(Despesas despesa) {
+		int count = 0;
+		for (long i = 1; i <= service.buscarTodos().size(); i++) {
+			if (!service.buscarPorId(i).getCusto().equals(new BigDecimal("0.00"))) {
+				count++;
+			} 
+		}
+		//BigDecimal convertDespesa = new BigDecimal(despesa.getDespesa());
+		System.out.println("Count: "+ count+" Despesa: "+despesa.getDespesa());
+		
+		//BigDecimal divisao = convertDespesa.divide(new BigDecimal(count));
+		rateio = despesa.getDespesa().setScale(0, RoundingMode.HALF_EVEN);
+		//ERRO NA LINHA 90 DESPESAS CONTROLLER
+		rateio = rateio.divide(new BigDecimal(count)).setScale(0, RoundingMode.HALF_EVEN);
+		System.out.println("Rateio convertido: "+ rateio);
+		
+	
+		for (long i = 1; i <= service.buscarTodos().size(); i++) {
+			if (!service.buscarPorId(i).getCusto().equals(new BigDecimal("0.00"))) {
+				result = service.buscarPorId(i).getCusto().add(rateio).multiply((despesa.getMargem().add(new BigDecimal("1"))));
+				service.buscarPorId(i).setVenda(result);
+				service.editar(service.buscarPorId(i));
+			} 
+		}
+	}
+	
 
 }
--- ecommerce/src/main/java/com/ecommerce/domain/Despesas.java
+++ ecommerce/src/main/java/com/ecommerce/domain/Despesas.java
@@ -9,17 +9,11 @@ import javax.persistence.Table;
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.NumberFormat.Style;
 
-@SuppressWarnings("serial")
-@Entity
-@Table(name = "DESPESAS")
-public class Despesas extends AbstractEntity<Long> {
+
+public class Despesas {
 	
-	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
-	@Column(name = "despesa", columnDefinition = "DECIMAL(7,2) default 0.00")
 	private BigDecimal despesa;
-	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
-	@Column(name = "margem", columnDefinition = "DECIMAL(7,2) default 0.00")
-	private Integer margem;
+	private BigDecimal margem;
 	
 	
 	public BigDecimal getDespesa() {
@@ -28,13 +22,11 @@ public class Despesas extends AbstractEntity<Long> {
 	public void setDespesa(BigDecimal despesa) {
 		this.despesa = despesa;
 	}
-	public Integer getMargem() {
+	public BigDecimal getMargem() {
 		return margem;
 	}
-	public void setMargem(Integer margem) {
+	public void setMargem(BigDecimal margem) {
 		this.margem = margem;
 	}
 	
-	
-
 }
